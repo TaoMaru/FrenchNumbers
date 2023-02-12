35b21600a0ad181f1062240e10f1a32d62318288
TaoMaru <jackson_maria1@columbusstate.edu>
2023-02-11 22:34:28 -0500

    Add project files.

---
+++ frenchNumbers.vb
@@ -0,0 +1,14 @@
+﻿' Program Name: French Numbers
+'Developer: Maria Jackson
+'Date: Feb 11, 2023
+'Purpose: This program prompts user to select between numbers 1-5
+'         Once clicked, the French translation of the number is displayed
+'         The user can exit the program via the Exit btn at any time
+Public Class frmNumbers
+    Private Sub frmNumbers_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' form loads without min and max buttons in caption of window
+        MinimizeBox = False
+        MaximizeBox = False
+
+    End Sub
+End Class
